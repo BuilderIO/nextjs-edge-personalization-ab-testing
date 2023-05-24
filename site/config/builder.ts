15a5af44cff59c63a5a05898dd9807e1fe355e74
gustavocodess <gustavoh.g.s@hotmail.com>
2023-05-24 13:15:49 -0300

    remove autocomplete

--- site/config/builder.ts
+++ site/config/builder.ts
@@ -39,22 +39,6 @@ Builder.registerComponent(
   }
 )
 
-// Builder.registerComponent(
-//   dynamic(() => import('../components/algolia/AutoComplete')),
-//   {
-//     name: 'Algolia Autocomplete',
-//     image:
-//       'https://cdn.builder.io/api/v1/image/assets%2F1fa6810c36c54e87bfe1a6cc0f0be906%2Ff30fd90c31634e5ebb716fad734185f4',
-//     inputs: [
-//       {
-//         name: 'width',
-//         type: 'text',
-//         defaultValue: '250px',
-//       },
-//     ],
-//   }
-// )
-
 Builder.registerComponent(
   dynamic(() => import('../components/algolia/SearchGrid')),
   {
--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
-import '@algolia/autocomplete-theme-classic/dist/theme.css'
 import Cookies from 'js-cookie'
 import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
