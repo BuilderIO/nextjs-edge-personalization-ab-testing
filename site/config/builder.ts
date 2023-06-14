ef6901c57e25371bc8cc2e853cae2b570db949e8
Steve Sewell <steve@builder.io>
2023-06-14 12:51:14 -0700

    better algolia icon

--- site/config/builder.ts
+++ site/config/builder.ts
@@ -157,7 +157,7 @@ Builder.registerComponent(
   {
     name: 'Algolia Recs',
     image:
-      'https://cdn.builder.io/api/v1/image/assets%2F1fa6810c36c54e87bfe1a6cc0f0be906%2Ff30fd90c31634e5ebb716fad734185f4',
+      'https://tabler-icons.io/static/tabler-icons/icons-png/brand-algolia.png',
     inputs: [
       {
         name: 'collection',
